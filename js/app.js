b51c174c2dd80c4b350b7d44c0082a62b8ef2763
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2020-07-15 16:37:04 +0100

    Update app.js to add tip calculating functionality.

--- js/app.js
+++ js/app.js
@@ -1,145 +1,91 @@
-//Things I learned: How to dynamically include options into an HTML form
-//Things I changed: I created two functions to handle input. One for errors. One for calculating the tip.
-//I broke out the feedback into three different feeds. Before, all three would show for just one error.
-//Added a validator to only calculate if feedback wasn't received.
-//Added a toFixed method to the output
-// Made input disappear after submitting
-//Bug. I had setInterval in my code instead of setTimeout. 
-
-(function(){
-  
-//Set up a service array
-const services = [{
-  value: 1,
-  title: "great - 20%"
-},{
-  value: 2,
-  title: "good - 10%"
-},{
-  value: 3,
-  title: "bad - 2%"
-}]
+const tipChoice = document.getElementById("input-service");
+const totalBill = document.getElementById("input-bill");
+const totalSplit = document.getElementById("input-users");
+const loader = document.querySelector(".loader");
+const results = document.querySelector(".results");
+const nestedHtml = results.querySelectorAll("span");
+const feedback = document.querySelector(".feedback");
 
 
-  
-  const validateInput = function(billAmount, numUsers, selectedService){
-    
-   let isFeedback = false;
-   const feedback = document.querySelector('.feedback');
-    feedback.innerHTML = '';
+// Create three choice elements.
+const goodTip = document.createElement("option");
+goodTip.text = "Good  -20%";
+goodTip.value = 1;
 
-     if  (billAmount === "" || billAmount <="0"){
-        feedback.classList.add('showItem', 'alert-danger');
-        feedback.innerHTML += `<p>Bill amount cannot be blank</p>`
-        isFeedback = true;
+const okayTip = document.createElement("option");
+okayTip.text = "Okay  -10%";
+okayTip.value = 2;
+
+const badTip = document.createElement("option");
+badTip.text = "Bad  -5%";
+badTip.value = 3;
+
+// Add new choice options to the select element.
+const newChoice = [goodTip, okayTip, badTip];
+for (let i = 0; i < newChoice.length; i++) {
+  tipChoice.appendChild(newChoice[i]);
+}
+
+const submitButton = document.querySelector(".submitBtn");
+
+const calculateTip = (event) => {
+  event.preventDefault();
+  let billAmount = Number(totalBill.value);
+  let billSplit = Number(totalSplit.value);
+  let rating = Number(tipChoice.value);
+  // Check if th no. of people who shared it is more than zero, else display an alert.
+  if (billSplit > 0) {
+    let tip;
+    switch (true) {
+      case (rating === 1):
+        tip = (billAmount / billSplit) * 0.20;
+        break;
+      case (rating === 2):
+        tip = (billAmount / billSplit) * 0.10;
+        break;
+      case (rating === 3):
+        tip = (billAmount / billSplit) * 0.05;
+        break;
+      default:
+        console.log(`The rating is ${rating}`);
+    };
+    let newTotal = (tip * billSplit) + billAmount;
+    let personalBill = newTotal / billSplit;
+    results.children[2].style.display = "block";
+    //  Get the "$" elements nested in the divs and set them to the values of the operations.
+    nestedHtml[0].innerHTML = `: $${tip.toFixed(2)}`;
+    nestedHtml[2].innerHTML = `: $${newTotal.toFixed(2)}`;
+    nestedHtml[4].innerHTML = `: $${personalBill.toFixed(2)}`;
+    // check if the number of people who split the bill is 1 and hide the redundant (Each person owes statement)
+    if (billSplit == 1) {
+      results.children[2].style.display = "none";
     }
-    
-    if (numUsers <= "0"){
-      feedback.classList.add('showItem', 'alert-danger');
-      feedback.innerHTML += `<p>Number of users must be greater than zero</p>`;
-       isFeedback = true;
-    } 
-    
-   if (selectedService === "0"){
-     feedback.classList.add('showItem', 'alert-danger');
-     feedback.innerHTML += `<p>You must select a Service</p>`
-      isFeedback = true;
-   }
-    
-    setTimeout(function(){
-      feedback.classList.remove('showItem', 'alert-danger');
-    }, 10000);
-    
-    return isFeedback;
-      
-  }; // end validateInput
-  
-  const calculateTip = function(billAmount, numUsers, selectedService) {
-   
-    let percentTip = '';
-    if (selectedService === "1"){
-      percentTip = 0.2;
-    } else if (selectedService === "2"){
-      percentTip = 0.1;
-    } else {
-      percentTip = 0.02;
-    }
-    
-    const tipAmount = Number(billAmount)*percentTip;
-    const totalAmount = Number(billAmount) + Number(tipAmount);
-    const eachPerson = Number(totalAmount) / Number(numUsers);
-    
-    return [tipAmount, totalAmount, eachPerson];
-   
-    
+    // Load the gif by setting it's display to block and after at least five seconds, make it none.
+    //  Nest anothet timeout function that shows the results by setting it's display to block.
+    //  Inside the nested tieout, set the results display to none to hide it and also reset the input values.
+    setTimeout(() => {
+      loader.style.display = "none";
+      setTimeout(() => {
+        results.style.display = "none";
+        totalBill.value = "";
+        totalSplit.value = "";
+        tipChoice.value = 0;
+      }, 5000);
+      results.style.display = "block";
+    }, 5000);
+    loader.style.display = "block";
+    // feedback text-center alert text-capitalize p-1 showItem alert-danger
+  } else {
+    feedback.innerHTML = "Number of people who shared the bill must be more one or more."
+    setTimeout(() => {
+      feedback.style.display = "none";
+      feedback.classList.remove("showItem", "alert-danger");
+    }, 5000);
+    feedback.style.display = "block";
+    feedback.classList.add("showItem", "alert-danger");
   };
-  
- //FORM SETUP - ADD SERVICES
-services.forEach(function(service){
-  //create the option element
-  const option = document.createElement('option');
-  option.textContent = service.title;
-  option.value = service.value;
-  //select the select element from the DOM
-  const select = document.querySelector('#input-service');
-  select.appendChild(option);
-})
-  
- //FORM SETUP - ADD EVENT LISTENER AND FUNCTION CALLS
-  const inputForm = document.querySelector('form');
-  inputForm.addEventListener('submit', function(e){
-    
-  e.preventDefault();
-   
-  //grab elements from the DOM
-  const inputBill = document.querySelector('#input-bill');
-  const inputUsers = document.querySelector('#input-users');
-  const serviceValue = document.querySelector('#input-service');
- 
-   //get values from DOM elements
-  let billAmount = inputBill.value;
-  let  numUsers = inputUsers.value;
-  let selectedService = serviceValue.value;
-    
-  //get feedback if info is not validated  
-  const isFeedback = validateInput(billAmount, numUsers, selectedService);
-    
-    
-    
-   //calculated tip if info was validated
-    if (!isFeedback){
-        const loader = document.querySelector('.loader');
-        const resultsDOM = document.querySelector('.results');
-        const tipResultsDOM = document.querySelector('#tip-amount');
-        const totalAmountDOM = document.querySelector('#total-amount');
-        const eachPersonDOM = document.querySelector('#person-amount');
-      
-       //calculate results
-        const results = calculateTip(billAmount, numUsers, selectedService);
-       //show loader  
-       loader.classList.add('showItem');
-       // show results after 2 seconds
-       setTimeout(function(){
-        loader.classList.remove('showItem');
-        tipResultsDOM.textContent= `${results[0].toFixed(2)}`
-        totalAmountDOM.textContent= `${results[1].toFixed(2)}`
-        eachPersonDOM.textContent= `${results[2].toFixed(2)}`
-        resultsDOM.classList.add('showItem');
-      },2000)
-      
-      //clear values from DOM elements after 5 seconds
-      setTimeout(function(){
-        inputBill.value = '';
-        inputUsers.value = '';
-        serviceValue.value = 0;
-        resultsDOM.classList.remove('showItem');
-      }, 10000)
 
+};
 
-    } //end isFeedback statement
-  
-  }); //end eventListener for form
-  
-  
-})();
-
+// Wire up the submit button
+submitButton.addEventListener("click", calculateTip);
